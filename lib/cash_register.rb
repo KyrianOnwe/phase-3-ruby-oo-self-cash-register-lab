f6905e94c75ccdec6319bc3b3cc256f93f726984
Kyrian <dr.kyrian.onwe@gmail.com>
2022-08-13 20:10:46 -0500

    commit tree 2040dc129b3a7c8a835b5bed22ca6933743d5d9c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
+    def initialize
+        @total = 0            
+    end
 
+    attr_accessor :discount
+
+    def cash_register_with_discount(discount = 20)
+
+    end
+
+    
+end
